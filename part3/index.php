d0009d499f1dec281db4a86c35ab4d4be6646bd6
Marvin-H01Z <167389610+Marvin-H01Z@users.noreply.github.com>
2024-06-20 01:46:42 +0900

    Part1 - Part 3

---
+++ part3/index.php
@@ -0,0 +1,70 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+  <title>OOP Form</title>
+  <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@3.4.1/dist/css/bootstrap.min.css" integrity="sha384-HSMxcRTRxnN+Bdg0JdbxYKrThecOKuH5zCYotlSAcp1+c8xmyTe9GYg1l9a69psu" crossorigin="anonymous">
+</head>
+
+<body>
+  <div class="container mt-5">
+    <h3>OOP Form</h3>
+    <form action="" method="POST">
+      <div class="form-group">
+        <label for="name">Name:</label>
+        <input type="text" class="form-control" id="name" name="name" value="<?php echo isset($_POST['name']) ? $_POST['name'] : ''; ?>" required>
+      </div>
+      <div class="form-group">
+        <label for="age">Age:</label>
+        <input type="number" class="form-control" id="age" name="age" value="<?php echo isset($_POST['age']) ? $_POST['age'] : ''; ?>" required>
+      </div>
+      <button type="submit" class="btn btn-primary">Submit</button>
+    </form>
+    <hr>
+    <?php
+
+    //Class USer
+    class User
+    {
+      // Eigenschaften
+      private $name;
+      private $age;
+
+      // Konstruktor
+      public function __construct($name, $age)
+      {
+        $this->name = $name;
+        $this->age = $age;
+      }
+
+      // Methode, um den Namen zu erhalten
+      public function getName()
+      {
+        return $this->name;
+      }
+
+      // Methode, um das Alter zu erhalten
+      public function getAge()
+      {
+        return $this->age;
+      }
+    }   //End of User-Class
+
+
+
+    if ($_SERVER["REQUEST_METHOD"] == "POST") {
+      $name = $_POST["name"];
+      $age = $_POST["age"];
+
+      $user = new User($name, $age);
+
+      // Output the user details
+      echo "<h4>User Details:</h4>";
+      echo "<p>Name: " . $user->getName() . "</p>";
+      echo "<p>Age: " . $user->getAge() . "</p>";
+    }
+    ?>
+  </div>
+</body>
+
+</html>
